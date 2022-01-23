0665456a4fbba41a6c04af9cb2c9f44208016cb4
B. Baughan <brentbaughan1@gmail.com>
2022-01-23 18:29:28 -0500

    added password hashing on user creation

--- pages/api/user/index.ts
+++ pages/api/user/index.ts
@@ -1,14 +1,20 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import prisma from '../../../db/prisma'
+import { hash } from "bcryptjs";
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../../db/prisma";
 
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { email, name, password } = req.body;
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const { email, name } = req.body
-    const postResult = await prisma.user.create({
-        data: {
-            email: email,
-            name: name,
-        }
-    })
-    res.status(200).json(postResult)
+  const hashedPassword = await hash(password, 10);
+  const postResult = await prisma.user.create({
+    data: {
+      email: email,
+      name: name,
+      password: hashedPassword,
+    },
+  });
+  res.status(200).json(postResult);
 }
